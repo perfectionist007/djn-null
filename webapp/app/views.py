ad923ed0e7a3928add59e82a350d5c6a5e151eef
perfectionist007 <turash.chowdhury@gmail.com>
2024-12-05 16:24:09 +0600

    first commit

---
+++ webapp/app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views import View
+
+
+# Create your views here.
+
+class Main(View):
+    def get(self, request):
+        return render(request=request, template_name="app/main.html")
