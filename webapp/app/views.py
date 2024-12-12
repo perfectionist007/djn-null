5c10e7c831c9b1d2e795949c82d5520b220a99a8
perfectionist007 <turash.chowdhury@gmail.com>
2024-12-12 15:56:48 +0600

    latest

--- webapp/app/views.py
+++ webapp/app/views.py
@@ -7,3 +7,27 @@ from django.views import View
 class Main(View):
     def get(self, request):
         return render(request=request, template_name="app/main.html")
+    
+class Login(View):
+    def get(self, request):
+        return render(request=request, template_name="app/login.html")
+    
+class Logout(View):
+    def get(self, request):
+        # run operations
+        pass
+
+class Home(View):
+    def get(self, request):
+        return render(request=request, template_name="app/home.html")
+
+class ChatPerson(View):
+    def get(self, request):
+        return render(request=request, template_name="app/chat_person.html")
+       
+class Register(View):
+    def get(self, request):
+        return render(request=request, template_name="app/register.html")
+
+
+
